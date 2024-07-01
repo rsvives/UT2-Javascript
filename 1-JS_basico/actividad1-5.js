8a4ce3dcc9a11bd8164075e92b40893bf0049d5d
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-07-01 20:20:20 +0200

    constantes

---
+++ 1-JS_basico/actividad1-5.js
@@ -0,0 +1,29 @@
+// console.log('la suma es:', 2 + 2);
+// console.log('la resta es:', 5 - 2);
+// console.log('la multiplicación es:', 2 * 8);
+// console.log('la división:', 8 / 2);
+// console.log('el resto de dividir 13 entre 2 es:', 13 % 2);
+
+// console.log('Hola' + ' ' + 'Dr. Nick');
+
+let a = 10;
+let b = 5;
+let suma = a + b
+let resta = a - b
+let multiplicacion = a * b
+let division = a / b
+
+//SUMA
+console.log("la suma es:", suma);
+
+//RESTA
+console.log("la resta es:", resta);
+
+//MULTIPLicacion
+console.log("la multiplicacion es:", multiplicacion);
+
+//DIVISION
+console.log("la division es:", division);
+
+
+
---
+++ 1-JS_basico/actividad1-6.js
@@ -0,0 +1,3 @@
+const lluviaEnAsturias = true
+
+lluviaEnAsturias = false
