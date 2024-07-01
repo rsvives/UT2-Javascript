ea40b91d4a7e4d68012e674732cc58ca162db4a2
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-07-01 19:40:37 +0200

    ejercicio 4

--- 1-JS_basico/actividad1-3.js
+++ 1-JS_basico/actividad1-3.js
@@ -3,7 +3,5 @@ console.log('la resta es:', 5 - 2);
 console.log('la multiplicación es:', 2 * 8);
 console.log('la división:', 8 / 2);
 console.log('el resto de dividir 13 entre 2 es:', 13 % 2);
-2 + 2;
-console.log('Hola' + ' ' + 'Dr. Nick');
 
-console.log(true);
+console.log('Hola' + ' ' + 'Dr. Nick');
---
+++ 1-JS_basico/actividad1-4.js
@@ -0,0 +1,7 @@
+// 1.4.Crear un programa que muestre `"Hola, me llamo"` y el nombre de una persona almacenado en una variable.
+var nombre = "rodri"
+var apellidos = "afa-formacion"
+console.log('hola me llamo ' + nombre + ' ' + apellidos)
+console.log(`hola me llamo ${nombre} ${apellidos}`)
+console.log('hola me llamo ${nombre}')
+console.log("hola me llamo ${nombre}")
