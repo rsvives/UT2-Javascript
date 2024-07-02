0e926d24b5ad5b449afad51e50819c216adf44f8
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-07-02 17:09:52 +0200

    portero 😎

---
+++ 1-JS_basico/actividad1-9.js
@@ -0,0 +1,9 @@
+const mayoriaDeEdad = 18
+let edadUsuario = window.prompt('Introduce tu edad')
+
+
+if (edadUsuario >= mayoriaDeEdad) {
+    alert('bienvenid@, adelante')
+} else {
+    alert('no puedes pasar')
+}
