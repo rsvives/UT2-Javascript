534edd55d0d0037782c5a25cd3189bf6cbe29e02
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-07-02 18:02:59 +0200

    fibonacci

---
+++ 1-JS_basico/actividad1-11.js
@@ -0,0 +1,21 @@
+let tecla = 'a'
+
+if (tecla === 'a') {
+    //...
+}
+
+//else if else if....
+// si tecla es=== a
+//console.log( moverse a la izquierda )
+
+// si tecla es=== d
+//console.log( moverse a la derecha )
+
+// si tecla es=== w
+//console.log( saltar )
+
+// si tecla es=== s
+//console.log( agacharse )
+
+// si tecla no es ninguna de las anteriores
+//console.log( tecla no reconocida )
