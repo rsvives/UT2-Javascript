91ef2f46bb20dc4d557d9f602325d497d2e27189
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-07-02 16:14:47 +0200

    tipos de comparadores lógicos

---
+++ 1-JS_basico/actividad1-8.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * 
+ * 1.8 Operadores lógicos: crear un programa que contenga las siguientes variables:
+let num1 = 4  // number
+let num2 = "5" //string
+
+Utilizar todos los operadores lógicos dentro de varios `console.log()` y analizar el resultado.
+Una vez hecho,  crear la siguiente variable y utilizar los operadores de igualdad y desigualdad(sólo valor y valor y tipo) para experimentar sus diferencias
+ */
+
+
+//comparadores lógicos que 
+//no tienen en cuenta el tipo de dato:
+
+console.log(4 == "5")
+console.log(4 != "5")
+console.log(4 > "5")
+console.log(4 >= "5")
+console.log(4 < "5")
+console.log(4 <= "5")
+
+
+//comparadores lógicos que 
+//SI tienen en cuenta el tipo de dato:
+console.log(5 === "5")
+console.log(5 === 5)
+
+console.log(5 !== "5")
+console.log(5 !== 5)
