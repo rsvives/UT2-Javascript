d9b16f6fd9c14b3657a864b34048df3ea56bdb58
rsvives <r.serranvives@gmail.com>
2024-07-03 16:42:28 +0200

    martes 02/07

---
+++ 1-JS_basico/actividad1-12.js
@@ -0,0 +1,5 @@
+// del uno al 50
+for (let i = 0; i < 50; i++) {
+    console.log("Este es el numero: " , i+1);
+    
+}
---
+++ 1-JS_basico/actividad1-13.js
@@ -0,0 +1,14 @@
+// pares del 2 al 50
+for (let i = 2; i <=50; i+=2) {
+   console.log('par: ',i)
+}
+//impares:
+for (let i = 1; i <=50; i+=2) {
+   console.log('impar: ',i)
+}
+
+//TODO:falta mostrar los numero primos
+
+// i=i+1 -> i++
+
+// i=i+2  -> i+=2
