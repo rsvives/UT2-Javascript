b9cbe4d03bf9922ef4518bee4b7fa5caa50b528f
rsvives <r.serranvives@gmail.com>
2024-07-08 19:01:48 +0200

    .

--- 2-JS-funciones_arrays_objetos/actividad2-3-saludar.js
+++ 2-JS-funciones_arrays_objetos/actividad2-3-saludar.js
@@ -1,2 +1,19 @@
 //funcion que pide el nombre y el género, 
 //y en base al genero me saluda Sr., Sra, indefinido
+console.log('Saludar');
+
+
+function saludar( genero, nombre='Desconocido'){
+    let distintivo= 'Sr o Sra'
+
+    if(genero=== 'Hombre'){
+        distintivo='Sr';
+    }else if(genero==='Mujer'){
+        distintivo='Sra'
+    }
+    console.log('Hola '+ distintivo +' '+ nombre );
+}
+
+saludar('Mujer','Judit')
+saludar('Hombre','Rodri')
+saludar('fhasdfhs')
