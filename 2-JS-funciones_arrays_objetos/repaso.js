78885c793ca99b56884fe20b7279a43d59a12709
rsvives <r.serranvives@gmail.com>
2024-07-10 19:17:38 +0200

    repaso.js

---
+++ 2-JS-funciones_arrays_objetos/repaso.js
@@ -0,0 +1,11 @@
+/**
+ * Compra:
+ * Crear un programa de Javascript que pida por pantalla un producto
+ * En base al producto introducido, el programa ha de mostrar en la consola el precio de dicho producto
+ * 
+ * Los productos son:
+ * leche: 2€
+ * café: 3€
+ * cereales: 1.50€
+ * 
+ */
