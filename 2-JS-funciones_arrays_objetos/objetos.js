f952f36e91fa1708e8a3ea6792441eed7ec53e05
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-07-15 13:37:20 +0200

    ejercicios

---
+++ 2-JS-funciones_arrays_objetos/objetos.js
@@ -0,0 +1,16 @@
+function servirCaña() {
+    console.log('🍺')
+}
+
+let bar = {
+
+    nombre: 'Casa Pepe',
+    direccion: {
+        calle: 'gnal elorza',
+        numero: 53
+    },
+    servirCaña: servirCaña
+
+}
+
+
